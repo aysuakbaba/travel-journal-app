4fc3531ab7b8d7f79b469f948781ce6f16b5f60a
aysuakbaba <aysuakbabaa@gmail.com>
2023-01-07 02:03:16 +0300

    init commit

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import earth from './assets/earth.png'
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <img src={earth} alt='earthIcon'></img>
+            <h1>my travel journal.</h1>
+
+
+        </div>
+
+    )
+
+}
+
+
+export default Navbar
